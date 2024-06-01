93910b27696631243d9e5e3f5488c44c33e87b8f
admin <mutashev.daur2004@gmail.com>
2024-06-01 18:45:27 +0500

    BottomNavigation

--- app/src/main/java/com/example/focus_on/MainActivity.java
+++ app/src/main/java/com/example/focus_on/MainActivity.java
@@ -1,14 +1,55 @@
 package com.example.focus_on;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
+import com.example.focus_on.blocked.apps.BlockedAppsFragment;
+import com.example.focus_on.databinding.ActivityMainBinding;
+import com.example.focus_on.discipline.timer.FocusFragment;
+import com.example.focus_on.goals.GoalsFragment;
+import com.example.focus_on.notes.NotesFragment;
+import com.example.focus_on.profile.ProfileFragment;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new FocusFragment());
+        binding.bottomNavigationView.setBackground(null);
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.focus:
+                    replaceFragment(new FocusFragment());
+                    break;
+                case R.id.goals:
+                    replaceFragment(new GoalsFragment());
+                    break;
+                case R.id.blockedApps:
+                    replaceFragment(new BlockedAppsFragment());
+                    break;
+                case R.id.notes:
+                    replaceFragment(new NotesFragment());
+                    break;
+                case R.id.profile:
+                    replaceFragment(new ProfileFragment());
+                    break;
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout, fragment);
+        fragmentTransaction.commit();
     }
 }
---
+++ app/src/main/java/com/example/focus_on/discipline/timer/FocusFragment.java
@@ -0,0 +1,21 @@
+package com.example.focus_on.discipline.timer;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.focus_on.R;
+
+public class FocusFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_focus, container, false);
+    }
+}

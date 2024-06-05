f59ba2704377c786424792656b83ac713d3d2d9b
admin <mutashev.daur2004@gmail.com>
2024-06-05 18:52:44 +0500

    Tablayout

--- app/src/main/java/com/example/focus_on/discipline/timer/FocusFragment.java
+++ app/src/main/java/com/example/focus_on/discipline/timer/FocusFragment.java
@@ -3,19 +3,31 @@ package com.example.focus_on.discipline.timer;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.focus_on.R;
+import com.google.android.material.tabs.TabLayout;
 
 public class FocusFragment extends Fragment {
+    TabLayout modeTabs;
+    View v;
+    ViewPager2 viewPager2;
+    ModeTabsFragmentAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_focus, container, false);
+        v = inflater.inflate(R.layout.fragment_focus, container, false);
+        defineViews();
+        return v;
+
+    }
+
+    private void defineViews() {
+        modeTabs = v.findViewById(R.id.focusModes);
     }
 }
--- app/src/main/java/com/example/focus_on/discipline/timer/modes/TimerFragment.java
+++ app/src/main/java/com/example/focus_on/discipline/timer/modes/TimerFragment.java
@@ -1,4 +1,4 @@
-package com.example.focus_on.discipline.timer;
+package com.example.focus_on.discipline.timer.modes;
 
 import android.os.Bundle;
 
@@ -10,7 +10,12 @@ import android.view.ViewGroup;
 
 import com.example.focus_on.R;
 
-public class TimeFragmet extends Fragment {
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link TimerFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TimerFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -21,7 +26,7 @@ public class TimeFragmet extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public TimeFragmet() {
+    public TimerFragment() {
         // Required empty public constructor
     }
 
@@ -31,11 +36,11 @@ public class TimeFragmet extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment TimeFragmet.
+     * @return A new instance of fragment TimerFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TimeFragmet newInstance(String param1, String param2) {
-        TimeFragmet fragment = new TimeFragmet();
+    public static TimerFragment newInstance(String param1, String param2) {
+        TimerFragment fragment = new TimerFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -56,6 +61,6 @@ public class TimeFragmet extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_time_fragmet, container, false);
+        return inflater.inflate(R.layout.fragment_timer, container, false);
     }
 }

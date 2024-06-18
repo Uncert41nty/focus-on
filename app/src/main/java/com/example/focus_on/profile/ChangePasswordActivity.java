eb980d17426d1992b71359346cb6593518f8c4bb
Senpa1 <mutashev.daur2004@gmail.com>
2024-06-18 11:38:08 +0600

    Change password

--- app/src/main/java/com/example/focus_on/profile/ChangePasswordActivity.java
+++ app/src/main/java/com/example/focus_on/profile/ChangePasswordActivity.java
@@ -1,10 +1,12 @@
 package com.example.focus_on.profile;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +17,7 @@ import com.example.focus_on.authorisation.SignInActivity;
 import com.example.focus_on.user.User;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
@@ -25,10 +28,16 @@ public class ChangePasswordActivity extends AppCompatActivity {
     Button changePasswordButton;
     String currentPassword;
     String newPassword;
+    DatabaseReference db;
+    DatabaseReference passwordReference;
+    User u;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
+
+        db = FirebaseDatabase.getInstance().getReference();
+        passwordReference = db.child("accounts");
 
         defineViews();
         passwordChange();
@@ -49,14 +58,17 @@ public class ChangePasswordActivity extends AppCompatActivity {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 for (DataSnapshot userSnapshot:snapshot.getChildren()) {
-                    User u = userSnapshot.getValue(User.class);
+                    u = userSnapshot.getValue(User.class);
                     if (!currentPassword.equals(u.getPassword())) {
                         currentPasswordEditText.setError("Incorrect password! Please try again");
                     } else {
                         changePasswordButton.setOnClickListener(new View.OnClickListener() {
                             @Override
                             public void onClick(View v) {
-
+                                passwordReference.child(u.getKey()).child("password").setValue(newPassword);
+                                setResult(Activity.RESULT_OK);
+                                finish();
+                                Toast.makeText(ChangePasswordActivity.this, "Password updated!", Toast.LENGTH_SHORT).show();
                             }
                         });
                     }
